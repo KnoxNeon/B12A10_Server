71fb1c75dbd592eb151b08f505a3dff0e6c5fa43
KnoxNeon <iftey10@gmail.com>
2025-12-04 23:34:07 +0600

    add post form

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -24,6 +25,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/services', async(req, res)=>{
+      const data = req.body;
+      console.log(data);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
